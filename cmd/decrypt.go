1f8eb7d98e3dfe741edbc88160a6f9f4433a18e8
jack5341 <nedim.akar53411@gmail.com>
2021-09-07 00:21:30 +0200

    done for decrypt notes

--- cmd/decrypt.go
+++ cmd/decrypt.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
+	"path"
 
 	"github.com/minio/minio-go"
 	"github.com/spf13/cobra"
@@ -13,25 +16,47 @@ var decCmd = &cobra.Command{
 	Use:   "dec",
 	Short: "List your all encrypted files and notes.",
 	Run: func(cmd *cobra.Command, args []string) {
-		decrypt(cmd.Flag("fname").Value.String(), cmd.Flag("name").Value.String())
+		decrypt(cmd.Flag("name").Value.String())
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(decCmd)
-	decCmd.Flags().StringP("fname", "f", "", "Decrypt and show your file")
-	decCmd.Flags().StringP("name", "n", "", "Decrypt and show your note")
+	decCmd.Flags().StringP("name", "n", "", "Decrypt and show your note or file")
 }
 
-func decrypt(filename string, notename string) {
-	fmt.Println(filename, notename)
+func decrypt(name string) {
+	if name == "" {
+		panic("Please provide a name")
+	}
+
 	bucketName := os.Getenv("MINIO_BUCKET_NAME")
 
-	status, err := Client.GetObject(bucketName, "notes/note-1.asc", minio.GetObjectOptions{})
+	status, err := Client.GetObject(bucketName, name, minio.GetObjectOptions{})
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Print(status)
+	tempPath := "/tmp/" + path.Base(name)
+
+	localFile, err := os.Create(tempPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if _, err = io.Copy(localFile, status); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(tempPath)
+	cmd := exec.Command("gpg", "--decrypt", tempPath)
+	data, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(data))
 }
