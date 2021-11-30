026aa37ac29b9e76aee26a04e69d3b82a2792352
Nedim Akar <nedim.akar53411@gmail.com>
2021-12-01 00:55:35 +0100

    chore: added two flag and optimized

--- cmd/encryption.go
+++ cmd/encryption.go
@@ -17,6 +17,12 @@ limitations under the License.
 package cmd
 
 import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+
 	"github.com/spf13/cobra"
 )
 
@@ -25,20 +31,56 @@ var encCmd = &cobra.Command{
 	Use:   "enc",
 	Short: "Encrypt your data SHA256 or GPG key",
 	Long: `Encrypt your data SHA256 or GPG key.
-Store them in minio or S3 buckets.`,
+Store them in minIO or S3 buckets.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		gpg, _ := cmd.Flags().GetBool("gpg")
-		sha, _ := cmd.Flags().GetBool("sha")
-		keep, _ := cmd.Flags().GetBool("keep")
+		note, _ := cmd.Flags().GetString("note")
+		file, _ := cmd.Flags().GetString("file")
+		gpgID := os.Getenv("DOLLOP_GPG_ID")
 
-		var err error
+		// gpg, err := cmd.Flags().GetBool("gpg")
+		// sha, err := cmd.Flags().GetBool("sha")
+		// keep, err := cmd.Flags().GetBool("keep")
 
-		if gpg {
+		if note != "" {
+			cmd := exec.Command("gpg", "--encrypt", "-r", gpgID, "--armor")
 
+			stdin, err := cmd.StdinPipe()
+
+			if err != nil {
+				errors.New("occurred with a problem while encrypt string")
+			}
+
+			go func() {
+				defer stdin.Close()
+				io.WriteString(stdin, note)
+			}()
+
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				errors.New("occurred with a problem while combined output")
+			}
+
+			result := string(out)
+
+			fmt.Println("Encrypted note: ", result)
 		}
 
-		if sha {
+		if file != "" {
+			cmd := exec.Command("gpg", "--encrypt", "--armor", "-r", gpgID, file)
 
+			var err error
+			var stdout io.Reader
+			if stdout, err = cmd.StdoutPipe(); err != nil {
+				errors.New("occurred with a problem while encrypting file")
+			}
+
+			err = cmd.Start()
+			if err != nil {
+				errors.New("occurred with a problem while upload encrypted file")
+			}
+			defer cmd.Wait()
+
+			fmt.Println("successfully encrypted", stdout)
 		}
 	},
 }
@@ -48,16 +90,6 @@ func init() {
 	encCmd.Flags().BoolP("gpg", "g", false, "Use GPG to encrypt your data")
 	encCmd.Flags().BoolP("sha", "s", false, "Use SHA256 to encrypt your data")
 	encCmd.Flags().BoolP("keep", "m", false, "Store your data in minio")
-}
-
-func encryptWithGPG() {
-
-}
-
-func encryptWithSHA() {
-
-}
-
-func encryptWithKeep() {
-
+	encCmd.Flags().StringP("note", "n", "", "Encrypt plain text note")
+	encCmd.Flags().StringP("file", "f", "", "Encrypt your file or directory")
 }
