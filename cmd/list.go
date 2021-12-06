394014d0124f6cbb346ccbaa0564638a0059e9ad
Nedim Akar <nedim.akar53411@gmail.com>
2021-12-06 21:52:59 +0100

    chore: optimized list command with new utils

---
+++ cmd/list.go
@@ -0,0 +1,91 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	util "github.com/jack5341/super-dollop/utils"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/pterm/pterm"
+	"github.com/spf13/cobra"
+)
+
+type TableStruct struct {
+	Name         string
+	Size         int64
+	LastModified string
+	ContentType  string
+}
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "A brief description of your command",
+	Run: func(cmd *cobra.Command, args []string) {
+		detail, _ := cmd.Flags().GetBool("detail")
+
+		// Environments
+		endpoint := os.Getenv("MINIO_ENDPOINT")
+		accessKeyID := os.Getenv("MINIO_ACCESS_KEY")
+		secretAccessKey := os.Getenv("MINIO_SECRET_KEY")
+
+		if endpoint == "" || accessKeyID == "" || secretAccessKey == "" {
+			log.Fatal("Please set MINIO_ENDPOINT, MINIO_ACCESS_KEY, MINIO_SECRET_KEY")
+			return
+		}
+
+		minioClient, err := minio.New(endpoint, &minio.Options{
+			Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+			Secure: false,
+		})
+
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		list := minioClient.ListObjects(cmd.Context(), "dollop", minio.ListObjectsOptions{})
+
+		if detail {
+			var table [][]string
+			listTable := append(table, []string{"Element Name", "Content-Type", "Last Modified", "Size"})
+
+			for e := range list {
+				sizeOfFile := util.ConvertByte(float64(e.Size))
+				listTable = append(listTable, []string{e.Key, e.ContentType, e.LastModified.String(), sizeOfFile})
+			}
+
+			pterm.DefaultTable.WithHasHeader().WithData(listTable).Render()
+			return
+		}
+
+		for e := range list {
+			sizeOfFile := util.ConvertByte(float64(e.Size))
+			nameOfFile := e.Key
+			fmt.Println(nameOfFile + " ----- " + sizeOfFile)
+			return
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+	listCmd.Flags().Bool("detail", false, "List encrypted files with details")
+}
--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.16
 require (
 	github.com/keybase/go-crypto v0.0.0-20200123153347-de78d2cb44f4
 	github.com/minio/minio-go/v7 v7.0.16
+	github.com/pterm/pterm v0.12.33
 	github.com/spf13/cobra v1.2.1
+	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
 )
